070240aa0bf5f24f93a5c128f8aacd10a7dee1d3
Huynh Nguyen Quoc Bao <hnquocbao22@gmail.com>
2022-04-04 20:20:07 +0700

    First commit

---
+++ app/src/main/java/com/example/tuan6_19503191_huynhnguyenquocbao/Product.java
@@ -0,0 +1,28 @@
+package com.example.tuan6_19503191_huynhnguyenquocbao;
+
+public class Product {
+
+    private int imgPro;
+    private String namePro;
+
+    public Product(int imgPro, String namePro) {
+        this.imgPro = imgPro;
+        this.namePro = namePro;
+    }
+
+    public int getImgPro() {
+        return imgPro;
+    }
+
+    public void setImgPro(int imgPro) {
+        this.imgPro = imgPro;
+    }
+
+    public String getNamePro() {
+        return namePro;
+    }
+
+    public void setNamePro(String namePro) {
+        this.namePro = namePro;
+    }
+}
